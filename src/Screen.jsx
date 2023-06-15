3d994e32af187624c07c1dc665c0ef99f3327e18
Artem Yakovenko <yakovenko.art1@gmail.com>
2023-06-15 13:16:23 +0500

    ne

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -2,8 +2,11 @@ import "./Screen.scss";
 
 function Screen({ screenValue }) {
   return (
-    <div>
-      <p className="input">{screenValue}</p>
+    <div className="wrapper">
+      <p className={`screen ${screenValue.length > 14 ? `screen__15` : ``}`}>
+        {screenValue}
+        <span className="ankor">1</span>
+      </p>
     </div>
   );
 }
